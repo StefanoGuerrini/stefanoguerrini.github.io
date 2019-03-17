3f7c5f4e100706fb731c3a1ff0c3884353941913
Stefano Guerrini <guerrinistefano3@gmail.com>
2019-03-17 22:49:40 +0100

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -22,3 +22,10 @@ git push origin master
 
 # Come Back up to the Project Root
 cd ..
+git add .
+msg="changes `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+git push origin master
